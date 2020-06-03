07dc15e63f1210892c2425b2fd17e39500401d6d
Carl Schriever <crschriever@gmail.com>
2020-06-03 13:40:15 -0400

    Updates

---
+++ precache-manifest.cd80dc3e8ba07a7f496c6754f8c52f5d.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd2fa39259256b0f9d4b21dfd6e5ad2",
+    "url": "/IBSpanishRubric/index.html"
+  },
+  {
+    "revision": "ccd61c918b11884405fa",
+    "url": "/IBSpanishRubric/static/js/main.c7bdcbee.chunk.js"
+  },
+  {
+    "revision": "d9e3c863aedc8a38c658",
+    "url": "/IBSpanishRubric/static/js/runtime-main.fca47457.js"
+  }
+]);
